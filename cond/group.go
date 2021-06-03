39e39f6234a4a581cf1ac3e00885fd0956baa6e1
Kycklingar <apanalex@hotmail.com>
2021-06-03 23:50:10 +0200

    export group

--- cond/group.go
+++ cond/group.go
@@ -2,23 +2,23 @@ package cond
 
 import "fmt"
 
-func Group() *group {
-	return new(group)
+func NewGroup() *Group {
+	return new(Group)
 }
 
-type group struct {
+type Group struct {
 	conds []Evaluator
 	ops   []string
 }
 
-func (c *group) Add(op string, cond Evaluator) *group {
+func (c *Group) Add(op string, cond Evaluator) *Group {
 	c.conds = append(c.conds, cond)
 	c.ops = append(c.ops, op)
 
 	return c
 }
 
-func (c *group) Eval(param *int) string {
+func (c *Group) Eval(param *int) string {
 	var ret string
 	for i := 0; i < len(c.conds); i++ {
 		ret += fmt.Sprintf("%s %s\n", c.ops[i], c.conds[i].Eval(param))
